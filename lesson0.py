635bad2f7f5a6b99735f7ce075ffe949b626d3d6
Mamariia <148610516+Mamariia@users.noreply.github.com>
2024-04-26 22:01:53 +0500

    Add files via upload

---
+++ lesson0.py
@@ -0,0 +1,7 @@
+stroka = 'это моя строка'
+print ('1) ' + stroka[0])
+print ('2) ' + stroka[-1])
+print ('3) ' + stroka[2:5])
+print ('4) ' + stroka[::-1])
+print ('5) ', len(stroka))
+print ('6) ' + 'это новая строка ' + stroka)
